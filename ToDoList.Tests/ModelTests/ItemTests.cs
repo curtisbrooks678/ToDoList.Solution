69850f98202847394eae8befafcd634526f15f46
Aaron Minnick <abminnick@gmail.com>
2021-12-08 09:33:02 -0800

    add static List of instances to Item.cs and tests
    Co-authored-by: Christy Welch <christinamawel@gmail.com>
    Co-authored-by: Curtis Brooks <curtisbrooks678@gmail.com>

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -1,11 +1,19 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
 using ToDoList.Models;
 
 namespace ToDoList.Tests
 {
   [TestClass]
-  public class ItemTests
+  public class ItemTests : IDisposable
   {
+
+    public void Dispose()
+    {
+      Item.ClearAll();
+    }
+
     [TestMethod]
     public void ItemConstructor_CreatesInstanceOfItem_Item()
     {
@@ -31,6 +39,26 @@ namespace ToDoList.Tests
       newItem.Description = updatedDescription;
       string result = newItem.Description;
       Assert.AreEqual(updatedDescription, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_ItemList()
+    {
+      List<Item> newList = new List<Item> {};
+      List<Item> result = Item.GetAll();
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsItems_ItemList()
+    {
+      string descriptionOne = "Walk the dog.";
+      string descriptionTwo = "Wash the dishes";
+      Item newItemOne = new Item(descriptionOne);
+      Item newItemTwo = new Item(descriptionTwo);
+      List<Item> newList = new List<Item> { newItemOne, newItemTwo };
+      List<Item> result = Item.GetAll();
+      CollectionAssert.AreEqual(newList, result);
     }
   }
 }
