fbf84344d8116161f7d4c0d2814c7e0eb840f1e8
Aaron Minnick <abminnick@gmail.com>
2021-12-08 10:13:22 -0800

    add ability to add items to list in main method
    Co-authored-by: Christy Welch <christinamawel@gmail.com>
    Co-authored-by: Curtis Brooks <curtisbrooks678@gmail.com>

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -5,6 +5,31 @@ public class Program
 {
   public static void Main()
   {
-    
+    Console.WriteLine("Welcome to the To Do List");
+    Console.WriteLine("Would you like to add an item to your list or view your list? (Add/View)");
+
+    string response = Console.ReadLine().ToLower();
+
+    if (response == "add") 
+    {
+      AddItem();
+    }
+    else if (response == "view")
+    {
+      // ViewList();
+    }
+    else 
+    {
+      Console.WriteLine("Sorry, I didn't quite understand you.");
+      Main();
+    }
+  }
+
+  static void AddItem()
+  {
+    Console.WriteLine("Please enter an item");
+    string description = Console.ReadLine();
+    Item newItem = new Item(description);
+    Main();
   }
 }
