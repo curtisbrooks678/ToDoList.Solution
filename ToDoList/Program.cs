2be632a5d691dd5c22d6295f4244b805268d5b5c
Curtis Brooks <curtisbrooks678@gmail.com>
2021-12-14 22:44:11 -0800

    add .gitignore

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,52 +1,20 @@
-using System;
-using ToDoList.Models;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
-public class Program
+namespace ToDoList
 {
-  public static void Main()
+  public class Program
   {
-    Console.WriteLine("Welcome to the To Do List");
-    Console.WriteLine("Would you like to add an item to your list or view your list? (Add/View)");
-
-    string response = Console.ReadLine().ToLower();
-
-    if (response == "add") 
+    public static void Main(string[] args)
     {
-      AddItem();
-    }
-    else if (response == "view")
-    {
-      ViewList();
-    }
-    else 
-    {
-      Console.WriteLine("Sorry, I didn't quite understand you.");
-      Main();
-    }
-  }
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .Build();
 
-  static void AddItem()
-  {
-    Console.WriteLine("Please enter an item");
-    string description = Console.ReadLine();
-    Item newItem = new Item(description);
-    Main();
-  }
-
-  static void ViewList()
-  {
-    if (Item.GetAll().Count == 0) {
-      Console.WriteLine("No items in list. Please add items.");
+      host.Run();
     }
-    else 
-    {
-      int counter = 1;
-      foreach (Item item in Item.GetAll())
-      {
-        Console.WriteLine(counter.ToString() + ". " + item.Description);
-        counter++;
-      }
-    }
-    Main();
   }
 }
